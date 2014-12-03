0ce3b2f50161c256aba8f74b4db9f816eb5b0f89
Rebecca Deubler <rebeccadeubler@dn0a233362.sunet>
2014-12-02 16:18:53 -0800

    Rebecca branch

--- src/Announcement.java
+++ src/Announcement.java
@@ -1,11 +1,15 @@
 package project;
 
+/**
+ * Announcement class - creates a new Announcement
+ * with specified from and text fields.
+ * @author rebeccadeubler
+ *
+ */
+
 public class Announcement extends Message{
 	
-	/*
-	 * inherited class to put announcements in database
-	 * inherits from message, which is the main ctor.
-	 */
+
 	public Announcement(String from, String text) {
 		super(null, from, text, null, ANNOUNCEMENT);
 	}
